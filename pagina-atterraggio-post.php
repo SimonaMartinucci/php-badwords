74b04082b3e5706f8669ca23121e749a6758b2ac
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-07-23 14:46:21 +0200

    feat: add form, add variables and output landing page (exercise ended)

--- pagina-atterraggio-post.php
+++ pagina-atterraggio-post.php
@@ -1,3 +1,22 @@
+<?php
+// valori del form
+$text = $_POST["text"];
+$badword = $_POST["badword"];
+
+// lunghezza testo
+$text_length = strlen($text);
+
+// cambio parolaccia scelta con ***
+$new_text = str_replace($badword, '***', $text);
+
+// lunghezza testo censurato
+$new_text_length = strlen($new_text);
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +25,12 @@
     <title>php-badwords-post</title>
 </head>
 <body>
+
+<p><?php echo $text ?></p>
+<span><?php echo $text_length ?></span>
+
+<p><?php echo $new_text ?></p>
+<span><?php echo $new_text_length ?></span>
     
 </body>
 </html>
